6a96672d4f6d3cf5f5cef21924afafb496babf41
StandardNerd <joon.ch@gmail.com>
2013-02-17 17:23:30 -0600

    add angular.js tutorial and oop js

---
+++ javascript/1d_oojs.js
@@ -0,0 +1,15 @@
+Not everything in Javascript are objects
+
+Primitive Values:
+// String
+// // Number
+// // Boolean
+// // Undefined
+// //Null
+//
+// Objects:
+// // Object
+// // Array
+// // Function
+// // Date
+// // RegExp
