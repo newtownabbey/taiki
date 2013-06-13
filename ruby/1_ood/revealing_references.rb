59bb21fdda0c7785e88a7598cf85dffc22b34ae0
choi <joon.ch@gmail.com>
2013-06-13 09:49:15 +0200

    simplify method diameters by separating it into two methods, each with one responsibility: iterate over the wheels and calculate the diameter of each wheel

--- ruby/1_ood/revealing_references.rb
+++ ruby/1_ood/revealing_references.rb
@@ -4,13 +4,21 @@ class RevealingReferences
     @wheels = wheelify(data)
   end
 
+  # first - iterate over the array
   def diameters
-    wheels.collect {|wheel| wheel.rim + (wheel.tire * 2)}
+    wheels.collect {|wheel| diameter(wheel)}
   end
-  # now everyone can send rim/tire to wheel
+
+  # second - calculate diameter of ONE wheel
+  def diameter(wheel)
+    wheel.rim + (wheel.tire * 2)
+  end
+
   
   Wheel = Struct.new(:rim, :tire)
   def wheelify(data)
     data.collect {|cell| Wheel.new(cell[0], cell[1])}
   end
 end
+
+
