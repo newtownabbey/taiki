0cb4a6c735b52d19d2620a6ff18e5ee08980079f
choi <joon.ch@gmail.com>
2013-06-13 12:15:07 +0200

    separate wheel methods in wheel class

--- ruby/1_ood/gear.rb
+++ ruby/1_ood/gear.rb
@@ -1,9 +1,11 @@
+require_relative 'wheel'
+
 class Gear
     attr_reader :chainring, :cog, :wheel
-    def initialize(chainring, cog, rim, tire)
+    def initialize(chainring, cog, wheel=nil)
         @chainring = chainring
         @cog       = cog
-        @wheel     = Wheel.new(rim, tire)
+        @wheel     = wheel
     end
 
     def ratio
@@ -14,13 +16,12 @@ class Gear
         ratio * wheel.diameter
     end
 
-    Wheel = Struct.new(:rim, :tire) do
-      def diameter 
-       rim + (tire * 2)
-      end
-    end
 end
 
-puts Gear.new(52, 11, 26, 1.5).ratio
-puts Gear.new(30, 27, 29, 1.5).gear_inches
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+puts Gear.new(30, 27).ratio
 
