4a1385221dd52dfe0d3f5b5a7e2f7445bd080059
choi <joon.ch@gmail.com>
2013-06-21 15:23:17 +0200

    add rails generate controller hello index

---
+++ hello01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello01::Application.config.secret_token = '55c1511bd927cc174c3e4dcbebde4fcd38984665083640e9830755474a5fbd5c6807d60b1836a68bc9399a2a497ff050e2e3acad683b48cda31040dd7233b678'
