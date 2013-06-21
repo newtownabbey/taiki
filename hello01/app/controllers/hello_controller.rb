bb6ef5ebf96e0ff04cbdc930c3edc7bb63a69c34
choi <joon.ch@gmail.com>
2013-06-21 15:42:03 +0200

    change layout to standardLayout

--- hello01/app/controllers/hello_controller.rb
+++ hello01/app/controllers/hello_controller.rb
@@ -1,4 +1,6 @@
 class HelloController < ApplicationController
+  layout "standardLayout" # instead of looking for views/layouts/hello.html.erb to be the layout, Rails will now look for views/layouts/standardLayout.html.erb
+  
   def index
     @message = 'Hello!'
     @count = 3
